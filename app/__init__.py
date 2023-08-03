032acfb22fe524f01eef8e07e8962d6dcfb3ba19
christine parsons <clong9712@gmail.com>
2023-08-02 20:44:44 -0400

    started phase 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+import random
+
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+@app.route('/')
+def index():
+    # return '<h1> HELLO </h1>'
+    return render_template('index.html', tweet=tweets[random.randint(0, len(tweets)-1)])
